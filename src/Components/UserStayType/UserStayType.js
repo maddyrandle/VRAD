0ef8f0ab95a65bd91fdd5dc4a74cf7dd80311f8e
Madison Randle <madisonrandle25@gmail.com>
2020-05-13 16:07:12 -0500

    Add images to userType buttons

--- src/Components/UserStayType/UserStayType.js
+++ src/Components/UserStayType/UserStayType.js
@@ -7,14 +7,19 @@ class UserStayType extends Component {
       <section className="userStayType">
         <p>Please Choose a Booking Type</p>
         <section className="stayTypes">
-          <button className="buisnessBtn">Buisness
+          <button className="buisnessBtn">
+            Buisness
+            <img src="https://image.flaticon.com/icons/svg/857/857676.svg" />
 
           </button>
-          <button className="vacationBtn">Vacation
+          <button className="vacationBtn">
+            Vacation
+            <img className="vacationBtnImg" src="https://image.flaticon.com/icons/svg/2905/2905677.svg" />
 
           </button>
-          <button className="otherBtn">Other
-
+          <button className="otherBtn">
+            Other
+            <img className="otherBtnImg" src="https://image.flaticon.com/icons/svg/684/684834.svg" />
           </button>
         </section>
       </section>
