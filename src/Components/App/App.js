9bdf36f84eb0529df9b88829bba30eda7551e190
Justin Corbin <jcorbin2290@gmail.com>
2020-05-13 13:47:04 -0600

    Created initail build of app data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <main className="App">
+class App extends Component {
+   constructor() {
+     super();
+     this.state = {
+       stayType: '',
+       areas: []
+     }
+   }
 
-    </main>
-  );
+  componentDidMount = async () => {
+    let availableAreaResponse = await fetch('https://vrad-api.herokuapp.com/api/v1/areas')
+    let availableAreaData = await availableAreaResponse.json()
+
+    let fullAreaData = availableAreaData.areas.map(async (area) => {
+      var fullAreaResponse = await fetch(`https://vrad-api.herokuapp.com${area.details}`)
+      var fullAreaData =  await fullAreaResponse.json()
+    })
+   }
+
+
+   render() {
+     return (
+       <main className="App">
+       </main>
+     );
+   }
 }
 
 export default App;
