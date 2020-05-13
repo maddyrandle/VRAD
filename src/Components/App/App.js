7dde53eabe82f33c7df555d07c2ab38cf0170f72
Madison Randle <madisonrandle25@gmail.com>
2020-05-13 15:55:10 -0500

    Update import files to make components more modular

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import Title from '../Title/Title';
-import UserStayType from '../UserStayType/UserStayType';
-import UserLogin from '../UserLogin/UserLogin';
+import LoginContainer from '../LoginContainer/LoginContainer';
+import PageHeaderContainer from '../PageHeaderContainer/PageHeaderContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main className="App">
-        <section className="loginContainerWrapper">
-          <section className="loginContainer">
-            <Title />
-            <form>
-              <UserStayType />
-              <UserLogin />
-            </form>
-          </section>
-        </section>
+        <LoginContainer />
       </main>
     );
   }
 }
 
 export default App;
+
+
+// <section className="landingPageWrapper">
+//   <PageHeaderContainer />
+// </section>
