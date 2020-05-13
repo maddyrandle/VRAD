6b5dc13c2c59da6553c662ac0891f01bf0969b10
Madison Randle <madisonrandle25@gmail.com>
2020-05-13 12:26:31 -0500

    Add jsx elements to style MVP

---
+++ src/Components/UserLogin/UserLogin.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './UserLogin.css';
+
+class UserLogin extends Component {
+  render() {
+    return (
+      <section className="UserLogin">
+        <p className="UserLoginTitle">Login Below</p>
+        <div>
+          <input placeholder="username" type="text"/>
+          <input placeholder="password" type="password"/>
+        </div>
+        <button className="UserLoginBtn">Login</button>
+      </section>
+    );
+  }
+}
+
+export default UserLogin;
