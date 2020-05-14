2fe647e39ff0b2a8955190e6aeff097b12708ec2
Justin Corbin <jcorbin2290@gmail.com>
2020-05-14 16:41:19 -0600

    Changed props passed down into Userlogin

--- src/Components/LoginContainer/LoginContainer.js
+++ src/Components/LoginContainer/LoginContainer.js
@@ -4,14 +4,24 @@ import UserStayType from '../UserStayType/UserStayType';
 import UserLogin from '../UserLogin/UserLogin';
 import './LoginContainer.css';
 
-const LoginContainer = ({ getPurpose, pageLogin }) => {
+const LoginContainer = ({ getPurpose, pageLogin, setUserName, setPassword, stayType, validateUser, validatePassword }) => {
   return (
     <section className="loginContainerWrapper">
       <section className="loginContainer">
         <Title />
         <form>
-          <UserStayType getPurpose={ getPurpose }/>
-          <UserLogin pageLogin={ pageLogin }/>
+          <UserStayType
+            getPurpose={ getPurpose }
+          />
+          <UserLogin
+            pageLogin={ pageLogin }
+            pageLogin= { pageLogin }
+            setUserName={ setUserName }
+            setPassword={ setPassword }
+            stayType={ stayType }
+            validateUser={ validateUser}
+            validatePassword= { validatePassword }
+          />
         </form>
       </section>
     </section>
