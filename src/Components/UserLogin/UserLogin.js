b8e69f38557cd2c9ce74d8802e394350ebdfd05e
Madison Randle <madisonrandle25@gmail.com>
2020-05-14 14:33:06 -0500

    Update UserLogin state when username or password input is not empty

--- src/Components/UserLogin/UserLogin.js
+++ src/Components/UserLogin/UserLogin.js
@@ -2,15 +2,44 @@ import React, { Component } from 'react';
 import './UserLogin.css';
 
 class UserLogin extends Component {
+  constructor() {
+    super();
+      this.state = {
+        username: '',
+        password: ''
+      }
+  }
+
+  updateUserName = (event) => {
+    this.setState({ username: event.target.value })
+  }
+
+  updatePassword = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  preventDefault = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <section>
         <p className="UserLoginTitle">Login Below</p>
         <div>
-          <input placeholder="username" type="text"/>
-          <input placeholder="password" type="password"/>
+
+          <input onChange={ this.updateUserName }
+            placeholder="username"
+            type="text"
+            name="username"/>
+
+          <input onChange={ this.updatePassword }
+            placeholder="password"
+            type="password"
+            name="password"/>
+
         </div>
-        <button className="UserLoginBtn" disabled>Login</button>
+        <button className="UserLoginBtn" disabled={ !this.state.username.length && !this.state.password.length } onClick={ this.preventDefault }>Login</button>
       </section>
     );
   }
