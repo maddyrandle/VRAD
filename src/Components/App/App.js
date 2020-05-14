f103ba0f203a0fb7089b11d46dc5baa9c472eb1c
Madison Randle <madisonrandle25@gmail.com>
2020-05-13 19:00:44 -0500

    Add constructor, super, and initial state to App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,10 +4,41 @@ import PageHeaderContainer from '../PageHeaderContainer/PageHeaderContainer';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+      this.state = {
+        stayType: '',
+        areas: []
+      }
+  }
+  componentDidMount = async () => {
+    let availableAreaResponse = await fetch('https://vrad-api.herokuapp.com/api/v1/areas')
+    let availableAreaData = await availableAreaResponse.json()
+
+    availableAreaData.areas.map(async (area) => {
+      var fullAreaResponse = await fetch(`https://vrad-api.herokuapp.com${area.details}`)
+      var fullAreaDetails =  await fullAreaResponse.json()
+      area.details = fullAreaDetails
+
+    area.details.listings.map(async (listing) => {
+      let listingResponse = await fetch(`https://vrad-api.herokuapp.com${listing}`)
+      let fullListingData = await listingResponse.json()
+      area.details.listings = fullListingData
+    })
+    })
+    await this.setState({
+     areas: availableAreaData
+    })
+  }
+
+  getPurpose = (type) => {
+    this.setState({ stayType: type })
+  }
+
   render() {
     return (
       <main className="App">
-        <LoginContainer />
+        <LoginContainer getPurpose={ this.getPurpose } />
       </main>
     );
   }
