8517b3b4f7dba6061304a4c04a6874c453eceecf
Madison Randle <madisonrandle25@gmail.com>
2020-05-14 21:01:56 -0500

    Update h1 on inner text

---
+++ src/Components/ListingDetailsContainer/ListingDetailsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+
+const ListingDetailsContainer = () => {
+  return (
+    <h1>ListingDetailsContainer</h1>
+  );
+}
+
+export default ListingDetailsContainer;
