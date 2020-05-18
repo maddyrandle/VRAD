758e66119722b36d665b1e1066a3de7b1e9793c7
Justin Corbin <jcorbin2290@gmail.com>
2020-05-18 15:44:22 -0600

    Changed width of defualt display image for listing

--- src/Components/Property/Property.js
+++ src/Components/Property/Property.js
@@ -12,7 +12,7 @@ const Property = ({name, areaID, listingID, selectedArea}) => {
       <div className='property'>
         <p className="name">{name}</p>
         <p>IMAGE GOES HERE</p>
-        <img src={`${listingImg}`} className='listingImage' />
+        <img src={`${listingImg}`} className='defaultListingImage' />
         <Link to={ `/areas/${areaID}/listings/${listingID}` } className="propertyBtn" areaID={areaID} listingID={listingID}>More Details</Link>
       </div>
     </section>
