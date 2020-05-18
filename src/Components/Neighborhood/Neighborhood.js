c763ba4f3faa129d5be68afc4454a2f27575b688
Justin Corbin <jcorbin2290@gmail.com>
2020-05-18 15:59:04 -0600

    Changed img paths

--- src/Components/Neighborhood/Neighborhood.js
+++ src/Components/Neighborhood/Neighborhood.js
@@ -3,11 +3,14 @@ import './Neighborhood.css'
 import { Link } from 'react-router-dom'
 
 const Neighborhood = ({nickname, description, fullName, id}) => {
+  var neighborhoodImg = `/Images/${nickname}.jpeg`
+
   return (
     <section className="neighborhoodContainer">
       <div className='neighborhood'>
         <p className="fullName">{fullName}</p>
         <p>IMAGES GO HERE</p>
+        <img src={`${neighborhoodImg}`} className='neighborhoodImg' />
         <p className="description">{description}</p>
         <Link to={`/areas/${id}/listings`} className="neighborhoodBtn" id={id}>Checkout {nickname}</Link>
       </div>
