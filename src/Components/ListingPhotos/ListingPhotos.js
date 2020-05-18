bb0b6123ee6254005820acf428bf6ba2621bf80c
Justin Corbin <jcorbin2290@gmail.com>
2020-05-18 15:28:14 -0600

    centered listing photos

--- src/Components/ListingPhotos/ListingPhotos.js
+++ src/Components/ListingPhotos/ListingPhotos.js
@@ -4,7 +4,7 @@ import './ListingPhotos.css'
 const ListingPhotos = ({listingPhotos}) => {
   let imagesArray = [`/Images/${listingPhotos.listing_id}_a.jpg`, `/Images/${listingPhotos.listing_id}_b.jpg`, `/Images/${listingPhotos.listing_id}_c.jpg`]
   let listingImages = imagesArray.map(image => {
-    return (<img src={`${image}`} />)
+    return (<img src={`${image}`} className='listingImage' />)
   })
   return (
     <section className="listingPhotos">
