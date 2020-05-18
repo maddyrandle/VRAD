62671329836d8707f3bf70ce400b82867f09ed98
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 01:55:37 -0500

    Rename destructured props

--- src/Components/Property/Property.js
+++ src/Components/Property/Property.js
@@ -3,13 +3,12 @@ import './Property.css'
 import { Link } from 'react-router-dom'
 
 const Property = ({name, areaID, listingID}) => {
-  console.log(name, areaID, listingID);
   return (
     <section className="propertyContainer">
       <div className='property'>
         <p className="name">{name}</p>
         <p>IMAGE GOES HERE</p>
-        <Link to={ `/areas/${areaID}/listings/${listingID}` } className="propertyBtn" id={listingID}>More Details</Link>
+        <Link to={ `/areas/${areaID}/listings/${listingID}` } className="propertyBtn" areaID={areaID} listingID={listingID}>More Details</Link>
       </div>
     </section>
   )
