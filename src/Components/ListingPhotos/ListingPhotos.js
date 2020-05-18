7fe477b4e9611dcc6e28fe7df85ad881b8720e2b
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 01:54:43 -0500

    Create bones of ListingPhotos component

---
+++ src/Components/ListingPhotos/ListingPhotos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ListingPhotos.css'
+
+const ListingPhotos = () => {
+  return (
+    <section className="listingPhotos">
+      <h1>photos</h1>
+      <h1>photos</h1>
+    </section>
+  )
+}
+
+export default ListingPhotos;
