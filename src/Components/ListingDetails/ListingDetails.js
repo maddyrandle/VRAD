5fcdd1d7b5f318af45d7a1094c2a30711b5f2d14
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 15:33:11 -0500

    Join features array to render on propertyDeatils page

--- src/Components/ListingDetails/ListingDetails.js
+++ src/Components/ListingDetails/ListingDetails.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import PropertyFeatures from '../PropertyFeatures/PropertyFeatures';
 import './ListingDetails.css'
 
 
 const ListingDetails = ({name, address, beds, baths, costPerNight, features}) => {
+
   return (
     <section className="listingDetails">
       <h2>{name}</h2>
@@ -12,11 +12,11 @@ const ListingDetails = ({name, address, beds, baths, costPerNight, features}) =>
         <li>{beds} Beds</li>
         <li>{baths} Baths</li>
         <li>{costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})} Per Night</li>
-        <li>Features will go here</li>
+        <li className="propertyFeatures">{features.join(', ')}</li>
       </ul>
     </section>
   )
 }
-// <p>Features: { <PropertyFeatures features={features}/> }</p>
+
 
 export default ListingDetails;
