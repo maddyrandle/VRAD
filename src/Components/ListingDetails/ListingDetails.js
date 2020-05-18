7cf13ffe6c5c0161345f6328280862cd05a33195
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 01:53:09 -0500

    Destructure props

---
+++ src/Components/ListingDetails/ListingDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ListingDetails.css'
+
+const ListingDetails = ({name, address, beds, baths, costPerNight, features}) => {
+  return (
+    <section className="listingDetails">
+      <h1>details</h1>
+      <h1>details</h1>
+    </section>
+  )
+}
+
+export default ListingDetails;
