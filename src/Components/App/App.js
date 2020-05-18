319631474ef7f89f80aab9d2886aeae30a6aa389
Madison Randle <madisonrandle25@gmail.com>
2020-05-17 22:29:09 -0500

    Iterate through listings with Object.keys and replace each listings element with its listing data object

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,20 +19,26 @@ class App extends Component {
   }
 
   componentDidMount = async () => {
+    let listings;
     let availableAreaResponse = await fetch('https://vrad-api.herokuapp.com/api/v1/areas')
     let availableAreaData = await availableAreaResponse.json()
 
     availableAreaData.areas.map(async (area) => {
       var fullAreaResponse = await fetch(`https://vrad-api.herokuapp.com${area.details}`)
-      var fullAreaDetails =  await fullAreaResponse.json()
+      var fullAreaDetails =  await fullAreaResponse.json();
       area.details = fullAreaDetails
 
-    area.details.listings.map(async (listing) => {
-      let listingResponse = await fetch(`https://vrad-api.herokuapp.com${listing}`)
-      let fullListingData = await listingResponse.json()
-      area.details.listings = fullListingData
+      Object.keys(area.details).forEach(detailsProperty => {
+        if (detailsProperty === 'listings') {
+          area.details[detailsProperty].map(async (listing, index) => {
+            let listingResponse = await fetch(`https://vrad-api.herokuapp.com${listing}`)
+            let fullListingData = await listingResponse.json()
+            area.details[detailsProperty].splice(index, 1, fullListingData)
+          })
+        }
+      })
     })
-    })
+
     await this.setState({
      areas: availableAreaData
     })
@@ -86,6 +92,7 @@ class App extends Component {
                 return area.details.id === parseInt(id)
               })
               return <DefaultContainer
+
                 selectedArea={selectedArea}
                 renderCondition='selectedArea'
                 resetState={this.resetState}
