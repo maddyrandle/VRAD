602c59291c22ca9a5b1267b95bea4f3cf2218a5e
Justin Corbin <jcorbin2290@gmail.com>
2020-05-18 15:45:22 -0600

    Deleted placeholder p tags

--- src/Components/Property/Property.js
+++ src/Components/Property/Property.js
@@ -9,7 +9,6 @@ const Property = ({name, areaID, listingID, selectedArea}) => {
     <section className="propertyContainer">
       <div className='property'>
         <p className="name">{name}</p>
-        <p>IMAGE GOES HERE</p>
         <img src={`${listingImg}`} className='defaultListingImage' />
         <Link to={ `/areas/${areaID}/listings/${listingID}` } className="propertyBtn" areaID={areaID} listingID={listingID}>More Details</Link>
       </div>
