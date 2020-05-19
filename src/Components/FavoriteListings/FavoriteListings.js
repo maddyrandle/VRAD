5a6692e483d6632a9cd91910a2e6bcc9a5f79773
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 19:24:31 -0500

    Change Link to button element

---
+++ src/Components/FavoriteListings/FavoriteListings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FavoriteListings.css';
+
+const FavoriteListings = ({ currentState, resetState }) => {
+
+
+  return (
+    <h1>Favorites</h1>
+  );
+}
+
+
+export default FavoriteListings;
