25373546c96919dbcc1518fcb54a9ee6feba886e
Madison Randle <madisonrandle25@gmail.com>
2020-05-19 13:39:50 -0500

    Add Favorites jsx

--- src/Components/FavoriteListingsDetails/FavoriteListingsDetails.js
+++ src/Components/FavoriteListingsDetails/FavoriteListingsDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './FavoriteListingsDetails.css';
 
-const FavoriteListingsDetails = ({ favorites }) => {
+const FavoriteListingsDetails = ({ favorites, name, areaid, listingid }) => {
 
   if (!favorites.length) {
     return (
@@ -9,10 +9,17 @@ const FavoriteListingsDetails = ({ favorites }) => {
     )
   } else {
     return (
-      <h1>hi</h1>
+      <section className="propertyContainer">
+        <div className='property'>
+          <p className="name">{name}</p>
+          <p>IMAGE GOES HERE</p>
+          <button className="propertyBtn">More Details</button>
+        </div>
+      </section>
     )
   }
 }
 
 
 export default FavoriteListingsDetails;
+// <Link to={ `/areas/${areaid}/listings/${listingid}` } className="propertyBtn" areaID={areaid} listingID={listingid}>More Details</Link>
