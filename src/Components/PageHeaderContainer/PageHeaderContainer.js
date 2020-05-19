baadf6ffe77047aa4c30b05ae16c81663d6d56e0
Madison Randle <madisonrandle25@gmail.com>
2020-05-18 19:31:54 -0500

    Pass through ComponentDidMount method as props for each page conditional

--- src/Components/PageHeaderContainer/PageHeaderContainer.js
+++ src/Components/PageHeaderContainer/PageHeaderContainer.js
@@ -2,21 +2,24 @@ import React from 'react';
 import LandingPageGreeting from '../LandingPageGreeting/LandingPageGreeting';
 import Navigation from '../Navigation/Navigation';
 import SelectedNeighboodDetails from '../SelectedNeighborhoodDetails/SelectedNeighborhoodDetails'
+import FavoriteListings from '../FavoriteListings/FavoriteListings'
 import './PageHeaderContainer.css';
 
-const PageHeaderContainer = ({ currentState, renderCondition, selectedArea, resetState }) => {
+const PageHeaderContainer = ({ currentState, renderCondition, selectedArea, resetState, getData }) => {
 
   let topContainer;
   if (renderCondition === 'allAreas') {
-    topContainer = <LandingPageGreeting currentState={currentState}/>
+    topContainer = <LandingPageGreeting currentState={currentState} />
   } else if (renderCondition === 'selectedArea' || renderCondition === 'listingDetails') {
-    topContainer = <SelectedNeighboodDetails selectedArea={selectedArea}/>
+    topContainer = <SelectedNeighboodDetails selectedArea={selectedArea} />
+  } else if (renderCondition === 'favorites') {
+    topContainer = <FavoriteListings />
   }
 
   return (
     <React.Fragment>
       {topContainer}
-      <Navigation resetState={resetState}/>
+      <Navigation resetState={resetState} getData={getData} currentState={currentState} />
     </React.Fragment>
   );
 }
