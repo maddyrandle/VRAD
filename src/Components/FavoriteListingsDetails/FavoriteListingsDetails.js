167705f4df958b46b2413f6aa6ddcf2099bb72b6
Madison Randle <madisonrandle25@gmail.com>
2020-05-19 17:01:11 -0500

    Pass handleFavorites method through as props to FavoriteListingsDetails component and remove conditional for empty favorites array

--- src/Components/FavoriteListingsDetails/FavoriteListingsDetails.js
+++ src/Components/FavoriteListingsDetails/FavoriteListingsDetails.js
@@ -2,25 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './FavoriteListingsDetails.css';
 
-const FavoriteListingsDetails = ({ favorites, name, areaid, listingid }) => {
+const FavoriteListingsDetails = ({ favorites, name, areaid, listingid, handleFarovites }) => {
 
-  if (!favorites.length) {
-    return (
-      <h1>You do not have any listings saved.</h1>
-    )
-  } else {
-    return (
-      <section className="propertyContainer">
-        <div className='property'>
-          <p className="name">{name}</p>
-          <Link to={ `/areas/${areaid}/listings/${listingid}` } className="propertyBtn" areaID={areaid} listingID={listingid}>More Details</Link>
-        </div>
-      </section>
-    )
+  const handleFarovite = () => {
+    handleFarovites(areaid, listingid)
   }
+
+  return (
+    <section className="propertyContainer">
+      <div className='property'>
+        <p className="name">{name}</p>
+        <Link to={ `/areas/${areaid}/listings/${listingid}` } className="propertyBtn" areaID={areaid} listingID={listingid}>More Details</Link>
+        <button className="favoritePropertyBtn" onClick={ handleFarovite }>Favorite this Property</button>
+      </div>
+    </section>
+  )
 }
 
-
 export default FavoriteListingsDetails;
-// <p>IMAGE GOES HERE</p>
-// <button className="propertyBtn">More Details</button>
