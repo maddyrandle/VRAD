61427ef77331384e51f9da9d0c6c367f4339c480
Madison Randle <madisonrandle25@gmail.com>
2020-05-19 14:49:25 -0500

    Change ListingDetails to a class component

--- src/Components/ListingDetails/ListingDetails.js
+++ src/Components/ListingDetails/ListingDetails.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropertyFeatures from '../PropertyFeatures/PropertyFeatures';
 import './ListingDetails.css'
 
-const ListingDetails = ({areaid, listingid, name, address, beds, baths, costPerNight, features, handleFarovites}) => {
-  const handleFarovite = () => {
-    handleFarovites(areaid, listingid)
+class ListingDetails extends Component {
+  constructor({areaid, listingid, name, address, beds, baths, costPerNight, features, handleFarovites}) {
+    super();
+      this.state = {
+        favorited: false,
+      }
   }
 
-  return (
-    <section className="listingDetails">
-      <h2>{name}</h2>
-      <p>{address.street}, CO, {address.zip}</p>
-      <ul className="propertyDetails">
-        <li>{beds} Beds</li>
-        <li>{baths} Baths</li>
-        <li>{costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})} Per Night</li>
-        <li>Features will go here</li>
-      </ul>
-      <button className="favoritePropertyBtn" onClick={ handleFarovite }>Favorite this Property</button>
-    </section>
-  )
+  handleFarovite = () => {
+    this.props.handleFarovites(this.props.areaid, this.props.listingid)
+  }
+
+  render() {
+    return (
+      <section className="listingDetails">
+        <h2>{this.props.name}</h2>
+        <p>{this.props.address.street}, CO, {this.props.address.zip}</p>
+        <ul className="propertyDetails">
+          <li>{this.props.beds} Beds</li>
+          <li>{this.props.baths} Baths</li>
+          <li>{this.props.costPerNight.toLocaleString("en-US", {style: "currency", currency: "USD"})} Per Night</li>
+          <li>Features will go here</li>
+        </ul>
+        <button className="favoritePropertyBtn" onClick={ this.handleFarovite }>Favorite this Property</button>
+      </section>
+    )
+  }
 }
+
 
 export default ListingDetails;
