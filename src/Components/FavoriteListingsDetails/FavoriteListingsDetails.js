b996c716ffadb6b7a78ae1417f220bf8c453ff50
Madison Randle <madisonrandle25@gmail.com>
2020-05-19 19:04:17 -0500

    New file in favorites PR - atom gave odd error message to save

---
+++ src/Components/FavoriteListingsDetails/FavoriteListingsDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './FavoriteListingsDetails.css';
+
+const FavoriteListingsDetails = ({ favorites, name, areaid, listingid, handleFarovites }) => {
+
+  const handleFarovite = () => {
+    handleFarovites(areaid, listingid)
+  }
+
+  return (
+    <section className="propertyContainer">
+      <div className='property'>
+        <p className="name">{name}</p>
+        <Link to={ `/areas/${areaid}/listings/${listingid}` } className="propertyBtn" areaID={areaid} listingID={listingid}>More Details</Link>
+        <button className="favoritePropertyBtn" onClick={ handleFarovite }>Favorite this Property</button>
+      </div>
+    </section>
+  )
+}
+
+export default FavoriteListingsDetails;
