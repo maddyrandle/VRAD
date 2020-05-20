988c4811ca71e70b41b907c1dba65c4db7834340
Justin Corbin <jcorbin2290@gmail.com>
2020-05-19 19:30:33 -0600

    Added initial test for APP

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitfor } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
+import { getListings } from '../../apiRequest'
+jest.mock('../../apiRequest')
+describe(App, () => {
+  const mockedData =
+    {area: 'RiNo',
+    details: {
+      id:590,
+      name:'River North',
+      location:'North of Downtown Denver',
+      about: 'Rino is Great',
+      listings:{
+        name: 'Hip RiNo Party Spot',
+        address: {street: '2250 lawrence drive', zip:'49494'},
+        area: 'rino',
+        area_id: 590,
+        details:{baths: 2.5, beds: 3, cost_per_night:420,
+        features:['hot tub', 'esspresso machine'],
+        listing_id: 3
+      }
+    }
+  }
+}
+  it('renders without crashing', () => {
+   const app = render(<App />)
+  });
+  it('should load the app with the correct data', () => {
+    const app = render(<App />)
+    getListings.mockResolvedValueOnce(mockedData)
+  });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+})
